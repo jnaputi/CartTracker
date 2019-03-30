02209ee55d83268347c6106aa20aa7d8f1d54a34
jnaputi <jnaputi253@gmail.com>
2019-03-30 10:11:23 -0500

    Removed unused tables

--- CartTracker/Models/Category.cs
+++ CartTracker/Models/Category.cs
@@ -7,11 +7,8 @@ namespace CartTracker.Models
     public class Category
     {
         public int CategoryId { get; set; }
-        public int? ItemId { get; set; }
         public string Name { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime LastUpdated { get; set; }
-
-        public Items Item { get; set; }
     }
 }
